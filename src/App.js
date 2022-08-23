ead9018a2901c174abb32486f4542e489139f7a7
Damir Gimatdinov <damirgim@yandex.com>
2022-08-23 18:34:44 +0500

    Adds classnames into the project, somehow dark mode works.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {createContext} from "react";
+import cn from "classnames/bind";
+import './style/resetStyle.css'
+import styles from './style/App.module.scss'
+import MyHeader from "./components/header/MyHeader";
+
+const cx = cn.bind(styles);
+export const ThemeContext = createContext(null);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, setTheme] = useState("light")
+    const toggleTheme = () => {
+        setTheme((curr) => curr === "light" ? "dark" : "light")
+    }
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <div className="App" id={theme}>
+                <div className={cx("wrapper", { "wrapper--dark": theme === "dark" })}>
+                    <div className={cx("wrapper__container")}>
+                        <MyHeader theme={theme} toggleTheme={toggleTheme}/>
+                    </div>
+                </div>
+            </div>
+        </ThemeContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/components/header/MyHeader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ReactComponent as Logo } from "./images/logo.svg";
+import { ReactComponent as BulbSwitchLight } from "./images/bulb_light.svg";
+import { ReactComponent as BulbSwitchDark } from "./images/bulb_dark.svg";
+import classes from "./MyHeader.module.scss";
+const MyHeader = ({ theme, toggleTheme}) => {
+    return (
+        <div className={classes.header}>
+            <Logo className={classes.header__logo}/>
+            <div className={classes.header__switch}>
+                {theme === "dark" ? (
+                    <BulbSwitchLight onClick={toggleTheme} />
+                ) : (
+                    <BulbSwitchDark onClick={toggleTheme} />
+                )}
+            </div>
+        </div>
+    )
+};
+export default MyHeader;
+
