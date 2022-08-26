2e92f787e59ca8d95fca7d4cdb5107677f98d618
Damir Gimatdinov <damirgim@yandex.com>
2022-08-27 00:12:41 +0500

    Adds adaptive for range selector

--- src/components/UI/range/MyRange.jsx
+++ src/components/UI/range/MyRange.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 import ArrowDropUpRoundedIcon from "@mui/icons-material/ArrowDropUpRounded";
 import cn from "classnames/bind";
-import styles from "../dropdown/MyDropDown.module.scss";
+import styles from "./MyRange.module.scss";
 import CloseIcon from "@mui/icons-material/Close";
 import MyInput from "../input/MyInput";
 
@@ -13,17 +13,17 @@ const MyRange = ({ filter, setFilter, theme }) => {
   return (
     <div
       className={cx(
-        "dropdown",
-        { dropdown_dark: theme === "dark" },
-        { dropdown_down_range: isActive }
+        "range",
+        { range_dark: theme === "dark" },
+        { range_down: isActive }
       )}
     >
-      <div className={cx("dropdown_btn")}>
-        <div className={cx("dropdown_text")}>Created</div>
-        <div className={cx("dropdown_buttons")}>
+      <div className={cx("range_menu")}>
+        <div className={cx("range_text")}>Created</div>
+        <div className={cx("range_buttons")}>
           {(filter.dateEnd || filter.dateStart) !== "" ? (
             <CloseIcon
-              className={cx("dropdown_buttons_close")}
+              className={cx("range_close")}
               onClick={(event) => {
                 setFilter({ ...filter, dateStart: "", dateEnd: "" });
                 setIsActive(!isActive);
@@ -38,20 +38,24 @@ const MyRange = ({ filter, setFilter, theme }) => {
           )}
         </div>
       </div>
-      <div className={cx("dropdown_content", "dropdown_content_range")}>
+      <div className={cx("range_content")}>
         <MyInput
           value={filter.dateStart}
           onChange={(e) => setFilter({ ...filter, dateStart: e.target.value })}
           placeholder={"from"}
           theme={theme}
-          className={cx("selector")}
+          className={cx("range_content_selector")}
         />
-        <div className={cx("divider", { divider_dark: theme === "dark" })} />
+        <div
+          className={cx("range_content_divider", {
+            range_content_divider_dark: theme === "dark",
+          })}
+        />
         <MyInput
           value={filter.dateEnd}
           onChange={(e) => setFilter({ ...filter, dateEnd: e.target.value })}
           placeholder={"before"}
-          className={cx("selector")}
+          className={cx("range_content_selector")}
           theme={theme}
         />
       </div>
